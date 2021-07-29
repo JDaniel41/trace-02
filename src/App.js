6688b693b539a26ebf5bc2a2bea2dbb2b75c8419
Jonathan Daniel <gcagrad19@gmail.com>
2021-07-28 23:26:08 -0400

    Refactored About Me

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutMe from "./components/AboutMe";
 import villagerImg from "./img/villager.jpg";
 import basketballImg from "./img/basketball.jpeg";
 import spotifyImg from "./img/spotify-logo.jpg";
+import soccerJonathan from "./img/soccer-jon.jpg";
 
 const sectionNames = ["Hero", "About Me"];
 const buttonData = [
@@ -32,6 +33,18 @@ const hobbyData = [
 			"I love playing basketball to relax! I never played on a team in High School, but you can catch me at Clemson playing 3v3 or 5v5 basketball all the time!",
 	},
 ];
+const aboutMeDetails = [
+	{
+		heading: "Mr. Robot",
+		description:
+			"In High School, I competed in FIRST Robotics on a team called Toaster Tech! We were bready to toast the competition!",
+	},
+	{
+		heading: "My Hometown",
+		description:
+			"Originally, I am from Stonecrest, Georgia, just outside of Atlanta, Georgia!",
+	},
+];
 
 function App() {
 	return (
@@ -43,7 +56,7 @@ function App() {
 				buttonData={buttonData}
 			/>
 			<Hobbies hobbyData={hobbyData} />
-			<AboutMe />
+			<AboutMe personPhoto={soccerJonathan} details={aboutMeDetails} />
 		</div>
 	);
 }
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,4 @@
 import react from "react";
-import soccerJonathan from "../img/soccer-jon.jpg";
 import AboutMeDetail from "./AboutMeDetail";
 
 function AboutMe(props) {
@@ -13,21 +12,21 @@ function AboutMe(props) {
 					<img
 						alt="feature"
 						className="object-cover object-center h-full w-full"
-						src={soccerJonathan}
+						src={props.personPhoto}
 					/>
 				</div>
 				<div
 					className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 
                 lg:pl-12 lg:text-left text-center justify-center"
 				>
-					<AboutMeDetail
-						heading="Mr. Robot"
-						description="In High School, I competed in FIRST Robotics on a team called Toaster Tech! We were bready to toast the competition!"
-					/>
-					<AboutMeDetail
-						heading="My Hometown"
-						description="Originally, I am from Stonecrest, Georgia, just outside of Atlanta, Georgia!"
-					/>
+					{props.details.map((feature) => {
+						return (
+							<AboutMeDetail
+								heading={feature.heading}
+								description={feature.description}
+							/>
+						);
+					})}
 				</div>
 			</div>
 		</section>
